816bf220c343e53ba43301089ff7342edc292e13
Soho36 <vladimir.sehirev@gmail.com>
2024-05-11 15:18:53 +0300

    start

---
+++ News/test.py
@@ -0,0 +1,8 @@
+class Author(models.Model):
+    full_name = models.CharField(max_length=64)
+    name = models.CharField(null=True, max_length=64)
+
+    def some_method(self):
+        self.name = self.full_name.split()[0]
+
+    self.save()
