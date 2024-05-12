a6dd4ec729468f22153d74c34a32fd517077fbdf
Soho36 <vladimir.sehirev@gmail.com>
2024-05-12 15:52:27 +0300

    ready models

--- News/portal/models.py
+++ News/portal/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_rating = models.IntegerField(default=0)
 
-    def like_dislike(self, like):
-        if like:
-            self.user_rating += 1
-        elif int(self.user_rating) > 0:  # Ensure rating doesn't go negative
-            self.user_rating -= 1
-        self.save()
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.comments = None
+        self.posts = None
 
     def update_rating(self):
-        pass
+        # Calculate total post rating
+        post_rating = (self.posts.aggregate(total_rating=Sum('post_rating'))['total_rating']) * 3 or 0
+
+        # Calculate total comment rating
+        comment_rating = self.comments.aggregate(total_rating=Sum('comment_rating'))['total_rating'] or 0
+
+        # Calculate total rating of comments on posts authored by the author
+        post_comments_rating = (
+            sum(comment.comment_rating for post in self.posts.all() for comment in post.comments.all())
+        )
+
+        # Update user rating
+        self.user_rating = post_rating + comment_rating + post_comments_rating
+        self.save()
+
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255, unique=True)
@@ -38,7 +51,6 @@ class Post(models.Model):
 
     def preview(self):
         beginning_of_article = self.post_text[:124] + ' ...'
-        self.save()
         return beginning_of_article
 
 
@@ -48,7 +60,7 @@ class PostCategory(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_text = models.TextField()
     comment_time = models.DateTimeField(auto_now_add=True)
@@ -60,3 +72,5 @@ class Comment(models.Model):
         elif int(self.comment_rating) > 0:  # Ensure rating doesn't go negative
             self.comment_rating -= 1
         self.save()
+
+
