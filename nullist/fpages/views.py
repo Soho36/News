f46020c951e0d2fcee5e2d464adbb35a55e22026
Soho36 <vladimir.sehirev@gmail.com>
2024-05-23 01:10:11 +0300

    some updates

---
+++ nullist/fpages/views.py
@@ -0,0 +1,6 @@
+# In myapp/views.py
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Welcome to the Home Page")
---
+++ pp2/simpleapp/views.py
@@ -0,0 +1,31 @@
+from django.views.generic import ListView, DetailView
+from .models import Product
+from django.http import HttpResponse
+from datetime import datetime
+from pprint import pprint
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    # queryset = Product.objects.filter(price__lt=1000)
+    template_name = 'products.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        pprint(context)
+
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+
+
+def home(request):
+    return HttpResponse("Welcome to the Home Page")
