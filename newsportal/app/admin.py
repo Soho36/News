073a3333fbf0344ec530fc450e592ed1ea9050ca
Soho36 <vladimir.sehirev@gmail.com>
2024-05-25 18:36:27 +0300

    working one

---
+++ newsportal/app/admin.py
@@ -0,0 +1,8 @@
+# Register your models here.
+
+from django.contrib import admin
+from .models import Category, News
+
+
+admin.site.register(Category)
+admin.site.register(News)
---
+++ newsportal/app/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import ListView, DetailView
+from .models import News
+
+
+class NewsList(ListView):
+    model = News
+    ordering = 'name'
+    template_name = 'news_list.html'
+    context_object_name = 'news_list'
+
+
+class NewsDetail(DetailView):
+    model = News
+    template_name = 'news_detail.html'
+    context_object_name = 'news_detail'
