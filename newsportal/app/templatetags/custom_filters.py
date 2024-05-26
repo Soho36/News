9c621046ef9aca5a0ef944bd25a6fc3d821166d1
Soho36 <vladimir.sehirev@gmail.com>
2024-05-26 21:22:15 +0300

    censored version

--- newsportal/app/templatetags/custom_filters.py
+++ newsportal/app/templatetags/custom_filters.py
@@ -4,14 +4,19 @@ register = template.Library()
 
 BAD_WORDS = {
     'fuck': 'f**k',
+    'Fuck!': 'F**k!',
+    'Fucking': 'F***ng',
     'fucking': 'f***ng',
     'shit': 's**t',
+    'Shit!': 'S**t!',
     'damn': 'd**n',
+    'goddamn': 'godd**n',
     # Add more words as needed
 }
+
 
 @register.filter(name='censor_bad_words')
 def censor_bad_words(value):
     words = value.split()
-    censored_words = [BAD_WORDS.get(word.lower(), word) for word in words]
+    censored_words = [BAD_WORDS.get(word, word) for word in words]
     return ' '.join(censored_words)
