7e9a646ae325f4d459d7ff8d033ae65835c57d4b
Soho36 <vladimir.sehirev@gmail.com>
2024-08-04 22:58:37 +0300

    night mode

--- newsportal/basic/context_processors.py
+++ newsportal/basic/context_processors.py
@@ -3,8 +3,11 @@ from django.utils import timezone
 
 
 def timezone_context(request):
+    current_time = timezone.now()
+    current_hour = current_time.astimezone(timezone.get_current_timezone()).hour
     return {
         'current_time': timezone.now(),
+        'current_hour': current_hour,
         'timezones': pytz.common_timezones,
         'TIME_ZONE': timezone.get_current_timezone_name()
     }
